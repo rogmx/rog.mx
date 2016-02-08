ab8f24a23aaa07986e6825c27f00c6a441fe8993
Rogelio Alberto <i@rog.mx>
2016-02-08 08:12:00 -0500

    Add Avatar and Logo

--- app/components/AboutCard/index.js
+++ app/components/AboutCard/index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
 import styles from './styles.css'
+import avatar from './images/me.png'
 
 function AboutCard () {
   return (
     <section className={styles.AboutCard}>
-      <div>
-        <p>
-          Made with love by <a href='https://github.com/rogr'>Rogelio</a>.
-        </p>
+      <div className={styles.AboutCard__Bio}>
+        <img src={avatar} className={styles.AboutCard__Bio__Avatar} />
+        <p>Made with love by <a href='https://github.com/rogr'>Rogelio</a>.</p>
       </div>
+      <div className={styles.AboutCard__SocialLinks} />
     </section>
   )
 }
