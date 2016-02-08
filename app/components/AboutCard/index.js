9fc305ed35ab4eba4857ad6bd01e4c30ee71a09c
Rogelio Alberto <i@rog.mx>
2016-02-08 08:12:00 -0500

    Add SVG Icons and Style

--- app/components/AboutCard/index.js
+++ app/components/AboutCard/index.js
@@ -1,33 +1,43 @@
 import React from 'react'
 
 import styles from './styles.css'
-import avatar from './images/me.png'
+
+import Avatar from './images/me.png'
+
+import IconTwitter from '../Icons/twitter'
+import IconGithub from '../Icons/github'
+import IconMusic from '../Icons/music'
 
 function AboutCard () {
   return (
     <section className={styles.AboutCard}>
       <div className={styles.AboutCard__Bio}>
-        <img src={avatar} className={styles.AboutCard__Bio__Avatar} />
+        <img src={Avatar} className={styles.AboutCard__Bio__Avatar} />
         <div className={styles.AboutCard__Bio__Info}>
           <h2 className={styles.AboutCard__Bio__Info__Title}>Rogelio Alberto</h2>
           <p className={styles.AboutCard__Bio__Info__Subtitle}>Web Developer</p>
         </div>
       </div>
       <div className={styles.AboutCard__SocialLinks}>
-        <ul>
-          <li>
-            <a href='http://twitter.com/rog3r'>
-              <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/social-twitter.svg' />
+        <ul className={styles.AboutCard__SocialLinks__List}>
+          <li className={styles.AboutCard__SocialLinks__List__Item}>
+            <a className={styles.AboutCard__SocialLinks__Link} href='http://twitter.com/rog3r'>
+              <IconTwitter className={styles.AboutCard__SocialLinks__Link__Icon} />
             </a>
           </li>
-          <li>
-            <a href='http://instagram.com/kantorpweglin'>
-              <img src='http://raziraz.github.io/codepen/img/instagram-icon.svg' />
+          <li className={styles.AboutCard__SocialLinks__List__Item}>
+            <a className={styles.AboutCard__SocialLinks__Link} href='http://github.com/rogr'>
+              <IconGithub className={styles.AboutCard__SocialLinks__Link__Icon} />
             </a>
           </li>
-          <li>
-            <a href='http://codepen.io/razitazi'>
-              <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/210284/social-codepen.svg' />
+          <li className={styles.AboutCard__SocialLinks__List__Item}>
+            <a className={styles.AboutCard__SocialLinks__Link} href='http://last.fm/rog3r'>
+              <IconMusic className={styles.AboutCard__SocialLinks__Link__Icon} />
+            </a>
+          </li>
+          <li className={styles.AboutCard__SocialLinks__List__Item}>
+            <a className={styles.AboutCard__SocialLinks__Link} href='#'>
+              <span className={styles.AboutCard__SocialLinks__Link__Text}>Blog</span>
             </a>
           </li>
         </ul>
