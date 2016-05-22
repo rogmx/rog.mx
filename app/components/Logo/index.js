c3eba05cf4c9b6b290de9dccb909b56c71846300
Rogelio Alberto <i@rog.mx>
2016-05-22 08:32:00 -0500

    Smaller Logo and Flip Animater Invader

--- app/components/Logo/index.js
+++ app/components/Logo/index.js
@@ -14,7 +14,8 @@ class Logo extends React.Component {
     super(props)
     this.state = {
       animated: false,
-      logoInterval: null
+      logoInterval: null,
+      flipInvader: false
     }
     this.mouseOver = this.mouseOver.bind(this)
     this.mouseOut = this.mouseOut.bind(this)
@@ -25,10 +26,12 @@ class Logo extends React.Component {
     const firstLogoAnimation = setInterval(() => {
       this.animateLogo()
       this.setState({animated: true})
+      this.setState({flipInvader: !this.state.flipInvader})
       firstLogoAnimationCount--
       if (firstLogoAnimationCount < 0) {
         clearInterval(firstLogoAnimation)
         this.setState({animated: false})
+        this.setState({flipInvader: false})
         this.animateLogo(true)
       }
     }, 435)
@@ -53,6 +56,7 @@ class Logo extends React.Component {
     this.animateLogo(false)
     this.state.logoInterval = setInterval(() => {
       this.animateLogo()
+      this.setState({flipInvader: !this.state.flipInvader})
     }, 435)
   }
 
@@ -60,6 +64,7 @@ class Logo extends React.Component {
     clearInterval(this.state.logoInterval)
     this.animateLogo(true)
     this.setState({animated: false})
+    this.setState({flipInvader: false})
   }
 
   render () {
@@ -70,8 +75,15 @@ class Logo extends React.Component {
           xmlns='http://www.w3.org/2000/svg'
           viewBox='0 0 55 310.5'>
           <g>
-            <path fill={'#fff'} stroke={'#010101'} d='M12.84 37.77v-4.18H8.66V42H2.39V29.41h4.18v-6.28h4.18V19H17v-4.23h-6.25V8.5H21.2V19h12.55V8.5H44.2v6.27h-6.27V19h6.27v4.18h4.18v6.27h4.18V42h-6.27v-8.41h-4.18v4.18h-4.18v7.71h4.18v6.4H31.66v-6.53h2.09v-7.58H21.2v7.84h2.16L23.29 52H12.9l-.06-6.34H17v-7.9h-4.16z'/>
-            <path fill={'#010101'} stroke={'#010101'} d='M17.09 24.7h6.27V31h-6.27v-6.3zm14.57 0h6.27V31h-6.27v-6.3z'/>
+            <path className={
+                this.state.flipInvader ? styles['Logo__Invader--flip'] : styles['Logo__Invader']
+              }
+              fill={'#fff'} stroke={'none'} d='M12.84 39.25v-5.66H8.66V42H2.39V29.41h4.18v-6.28h4.18V19H17v-4.23h-6.25V8.5H21.2V19h12.55V8.5H44.2v6.27h-6.27V19h6.27v4.18h4.18v6.27h4.18V42h-6.27v-8.41h-4.18v5.35H38l-.08 6.54h5.07v6.38H31.66v-6.51h2.09L33.66 39H21.2v6.58h2.16L23.29 52H11.58l.08-6.34H17l-.08-6.44z' />
+            <path className={
+                !this.state.flipInvader ? styles['Logo__Invader--flip'] : styles['Logo__Invader']
+              }
+              fill={'#fff'} stroke={'none'} d='M12.78 39.25v-5.66H8.6v-4.13H2.32V14.69h5.9v8.45h2.47V19H17v-4.23h-6.31V8.5h10.45V19h12.54V8.5h10.46v6.27h-6.27V19h6.27v4.18h2v-8.37h6.38V29.5h-6.29v4.09H42v5.35h-4.06l-.08 6.54h5.07v6.38H31.59v-6.51h2.09L33.59 39H21.14v6.58h2.16L23.23 52H11.52l.08-6.34H17l-.08-6.44z' />
+            <path fill={'#010101'} stroke={'#010101'} d='M17.09 24.7h6.27V31h-6.27v-6.3zm14.57 0h6.27V31h-6.27v-6.3z' />
           </g>
           <g id={'letter-r'} className={classNames({
             [styles.Logo__Letter]: true,
