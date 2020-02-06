3633950b3b7b4b7d42973b4a1d29f3d0eccf86e1
Rogelio Alberto <i@rog.mx>
2020-02-06 07:28:36 -0600

    Fix build

--- src/index.js
+++ src/index.js
@@ -1,22 +1,6 @@
-import { h, render } from 'preact' // eslint-disable-line
-
 import 'normalize.css/normalize.css'
 import './style'
 
-function init () {
-  let App = require('./components/app').default
-  render(<App />, document.body, document.getElementById('app'))
-}
+import App from './components/app'
 
-// register ServiceWorker via OfflinePlugin, for prod only:
-if (process.env.NODE_ENV === 'production') {
-  require('./pwa')
-}
-
-// in development, set up HMR:
-if (module.hot) {
-  // require('preact/devtools')
-  module.hot.accept('./components/app', () => requestAnimationFrame(init))
-}
-
-init()
+export default App
